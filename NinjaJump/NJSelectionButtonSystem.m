f84c9c4c7d7a9ff43f583068165ac3b9429a033b
Wang Yichao <wangyichao@r-202-179-28-172.comp.nus.edu.sg>
2014-03-24 19:49:54 +0800

    add spotlight animation to character selection scene

--- NinjaJump/NJSelectionButtonSystem.m
+++ NinjaJump/NJSelectionButtonSystem.m
@@ -13,37 +13,78 @@
 
 @implementation NJSelectionButtonSystem{
     NSMutableArray *selectionButtons;
+    NSMutableArray *spotLightList;
 }
 
 - (id) init{
     self = [super initWithImageNamed:@"ready buttons.png"];
     if (self) {
-        NJSelectCharacterButton *selectionButtonBlue = [[NJSelectCharacterButton alloc] initWithType:BLUE];
-        NJSelectCharacterButton *selectionButtonRed = [[NJSelectCharacterButton alloc] initWithType:RED];
-        NJSelectCharacterButton *selectionButtonBrown = [[NJSelectCharacterButton alloc] initWithType:BROWN];
-        NJSelectCharacterButton *selectionButtonPurple = [[NJSelectCharacterButton alloc] initWithType:PURPLE];
-        selectionButtons = [NSMutableArray arrayWithObjects:selectionButtonBlue, selectionButtonRed, selectionButtonPurple, selectionButtonBrown, nil];
-        for (NJSelectCharacterButton *selectionButton in selectionButtons) {
-            selectionButton.hidden = YES;
-            selectionButton.delegate = self;
-            [self addChild:selectionButton];
-        }
+        [self addStartButton];
+        [self addSelectionButtons];
+        [self addSpotlight];
     }
     return self;
+}
+
+- (void)addStartButton{
+    SKSpriteNode *startButton = [SKSpriteNode spriteNodeWithImageNamed:@"start button.png"];
+    SKSpriteNode *shade = [SKSpriteNode spriteNodeWithImageNamed:@"arena shade.png"];
+    [self addChild:shade];
+    [self addChild:startButton];
+    startButton.position = CGPointMake(30, 0);
+}
+
+- (void)addSpotlight{
+    spotLightList = [NSMutableArray array];
+    for (int i=0; i<4; i++) {
+        SKSpriteNode *spotLight = [SKSpriteNode spriteNodeWithImageNamed:@"spotlight.png"];
+        spotLight.hidden = YES;
+        [spotLightList addObject:spotLight];
+    }
+    CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
+    CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
+    CGFloat r = 90.0f;
+    ((SKSpriteNode *)spotLightList[0]).position = CGPointMake(r-screenHeight/2, screenWidth/2-r);
+    ((SKSpriteNode *)spotLightList[0]).xScale = -1;
+    ((SKSpriteNode *)spotLightList[1]).position = CGPointMake(r-screenHeight/2, r-screenWidth/2);
+    ((SKSpriteNode *)spotLightList[1]).xScale = -1;
+    ((SKSpriteNode *)spotLightList[1]).yScale = -1;
+    ((SKSpriteNode *)spotLightList[2]).position = CGPointMake(screenHeight/2-r, screenWidth/2-r);
+    ((SKSpriteNode *)spotLightList[3]).position = CGPointMake(screenHeight/2-r, r-screenWidth/2);
+    ((SKSpriteNode *)spotLightList[3]).yScale = -1;
+    NSLog(@"%lu",spotLightList.count);
+    for (int i=0; i<spotLightList.count; i++) {
+        [self addChild:spotLightList[i]];;
+    }
+}
+
+- (void)addSelectionButtons{
+    NJSelectCharacterButton *selectionButtonBlue = [[NJSelectCharacterButton alloc] initWithType:BLUE];
+    NJSelectCharacterButton *selectionButtonRed = [[NJSelectCharacterButton alloc] initWithType:RED];
+    NJSelectCharacterButton *selectionButtonBrown = [[NJSelectCharacterButton alloc] initWithType:BROWN];
+    NJSelectCharacterButton *selectionButtonPurple = [[NJSelectCharacterButton alloc] initWithType:PURPLE];
+    selectionButtons = [NSMutableArray arrayWithObjects:selectionButtonBlue, selectionButtonRed, selectionButtonPurple, selectionButtonBrown, nil];
+    for (NJSelectCharacterButton *selectionButton in selectionButtons) {
+        selectionButton.hidden = YES;
+        selectionButton.delegate = self;
+        [self addChild:selectionButton];
+    }
 }
 
 - (void)button:(NJSelectCharacterButton *) button touchesEnded:(NSSet *)touches{
     UITouch *touch = [touches anyObject];
     
     CGPoint touchPoint = [touch locationInNode:self];
-    NSLog(@"%f %f",touchPoint.x,touchPoint.y);
+//    NSLog(@"%f %f",touchPoint.x,touchPoint.y);
     //CGContextRef ctx = UIGraphicsGetCurrentContext();
     
     for (int i=0; i<4; i++) {
         CGPathRef path= [self pathOfButton:(NJSelectionButtonType)i];
         NJSelectCharacterButton *button = selectionButtons[i];
+        SKSpriteNode *spotLight = spotLightList[i];
         if (CGPathContainsPoint(path, &CGAffineTransformIdentity, touchPoint, YES)) {
             button.hidden = !button.hidden;
+            spotLight.hidden = !spotLight.hidden;
         }
         CGPathRelease(path);
     }
--- NinjaJump/Scenes/NJLevelSceneWaterPark.m
+++ NinjaJump/Scenes/NJLevelSceneWaterPark.m
@@ -73,7 +73,6 @@
     self.physicsWorld.contactDelegate = self;
     
     [self addBackground];
-    
     [self addWoodPiles];
 }
 
@@ -110,6 +109,21 @@
     NJPile *pile10 = [[NJPile alloc] initWithTextureNamed:@"woodPile" atPosition:CGPointMake(750, 150) withSpeed:0 angularSpeed:3 path:nil];
     [self addNode:pile10 atWorldLayer:NJWorldLayerBelowCharacter];
     [self.woodPiles addObject:pile10];
+    
+    //add in the birthplace pile of ninjas
+    CGFloat r= 120.0f;
+    NJPile *pile11 = [[NJPile alloc] initWithTextureNamed:@"woodPile" atPosition:CGPointMake(r, r) withSpeed:0 angularSpeed:3 path:nil];
+    [self addNode:pile11 atWorldLayer:NJWorldLayerBelowCharacter];
+    [self.woodPiles addObject:pile11];
+    NJPile *pile12 = [[NJPile alloc] initWithTextureNamed:@"woodPile" atPosition:CGPointMake(1024-r, r) withSpeed:0 angularSpeed:3 path:nil];
+    [self addNode:pile12 atWorldLayer:NJWorldLayerBelowCharacter];
+    [self.woodPiles addObject:pile12];
+    NJPile *pile13 = [[NJPile alloc] initWithTextureNamed:@"woodPile" atPosition:CGPointMake(1024-r, 768-r) withSpeed:0 angularSpeed:3 path:nil];
+    [self addNode:pile13 atWorldLayer:NJWorldLayerBelowCharacter];
+    [self.woodPiles addObject:pile13];
+    NJPile *pile14 = [[NJPile alloc] initWithTextureNamed:@"woodPile" atPosition:CGPointMake(r, 768-r) withSpeed:0 angularSpeed:3 path:nil];
+    [self addNode:pile14 atWorldLayer:NJWorldLayerBelowCharacter];
+    [self.woodPiles addObject:pile14];
 }
 
 
