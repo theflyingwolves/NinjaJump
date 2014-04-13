c05bca66c286a0dee7a0df09626862a30e16c47e
Wang Kunzhen <a0105514@nus.edu.sg>
2014-04-13 13:42:06 +0800

    NinjaCharacterBoss Implemented

---
+++ NinjaJump/Sprites/Characters/NJNinjaCharacterBoss.h
@@ -0,0 +1,13 @@
+//
+//  NJNinjaCharacterBoss.h
+//  NinjaJump
+//
+//  Created by Wang Kunzhen on 13/4/14.
+//  Copyright (c) 2014 Wang Kunzhen. All rights reserved.
+//
+
+#import "NJNinjaCharacter.h"
+
+@interface NJNinjaCharacterBoss : NJNinjaCharacter
+
+@end
--- NinjaJump/Sprites/Characters/NJNinjaCharacterBoss.m
+++ NinjaJump/Sprites/Characters/NJNinjaCharacterBoss.m
@@ -1,26 +1,27 @@
 //
-//  NJNinjaCharacterNormal.m
+//  NJNinjaCharacterBoss.m
 //  NinjaJump
 //
-//  Created by Wang Kunzhen on 18/3/14.
+//  Created by Wang Kunzhen on 13/4/14.
 //  Copyright (c) 2014 Wang Kunzhen. All rights reserved.
 //
-#define NUM_OF_FRAMES_FOR_NORMAL_NINJA_JUMP 17
-#define NUM_OF_FRAMES_FOR_NORMAL_NINJA_DEATH 10
-#define NUM_OF_FRAMES_FOR_NORMAL_NINJA_ATTACK 6
-#define NUM_OF_FRAMES_FOR_NORMAL_NINJA_THUNDER 6
 
-#import "NJNinjaCharacterNormal.h"
+#define NUM_OF_FRAMES_FOR_BOSS_NINJA_JUMP 17
+#define NUM_OF_FRAMES_FOR_BOSS_NINJA_DEATH 10
+#define NUM_OF_FRAMES_FOR_BOSS_NINJA_ATTACK 6
+#define NUM_OF_FRAMES_FOR_BOSS_NINJA_THUNDER 6
+
+#import "NJNinjaCharacterBoss.h"
 #import "NJGraphicsUnitilities.h"
 
-@implementation NJNinjaCharacterNormal
+@implementation NJNinjaCharacterBoss
 
 - (instancetype)initWithTextureNamed:(NSString *)textureName atPosition:(CGPoint)position withPlayer:(NJPlayer *)player
 {
     self = [super initWithTextureNamed:textureName atPosition:position withPlayer:player];
     if (self) {
-        self.magicalDamageMultiplier = 1.0f;
-        self.physicalDamageMultiplier = 1.0f;
+        self.magicalDamageMultiplier = 0.25f;
+        self.physicalDamageMultiplier = 0.25f;
     }
     return self;
 }
@@ -30,9 +31,9 @@
     [super loadSharedAssets];
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        sSharedJumpAnimationFrames = [NJGraphicsUnitilities NJLoadFramesFromAtlas:@"Ninja_Jump" withBaseName:@"ninja_jump_" andNumOfFrames:NUM_OF_FRAMES_FOR_NORMAL_NINJA_JUMP];
-        sSharedAttackAnimationFrames = [NJGraphicsUnitilities NJLoadFramesFromAtlas:@"Ninja_Attack" withBaseName:@"attack_light_" andNumOfFrames:NUM_OF_FRAMES_FOR_NORMAL_NINJA_ATTACK];
-        sSharedThunderAnimationFrames = [NJGraphicsUnitilities NJLoadFramesFromAtlas:@"Ninja_Thunder" withBaseName:@"ninja_thunder_" andNumOfFrames:NUM_OF_FRAMES_FOR_NORMAL_NINJA_THUNDER];
+        sSharedJumpAnimationFrames = [NJGraphicsUnitilities NJLoadFramesFromAtlas:@"Ninja_Jump" withBaseName:@"ninja_jump_" andNumOfFrames:NUM_OF_FRAMES_FOR_BOSS_NINJA_JUMP];
+        sSharedAttackAnimationFrames = [NJGraphicsUnitilities NJLoadFramesFromAtlas:@"Ninja_Attack" withBaseName:@"attack_light_" andNumOfFrames:NUM_OF_FRAMES_FOR_BOSS_NINJA_ATTACK];
+        sSharedThunderAnimationFrames = [NJGraphicsUnitilities NJLoadFramesFromAtlas:@"Ninja_Thunder" withBaseName:@"ninja_thunder_" andNumOfFrames:NUM_OF_FRAMES_FOR_BOSS_NINJA_THUNDER];
         //sSharedDeathAnimationFrames = [NJGraphicsUnitilities NJLoadFramesFromAtlas:@"ninja_normal_death" withBaseName:@"ninja_death_atlas_" andNumOfFrames:NUM_OF_FRAMES_FOR_NORMAL_NINJA_DEATH];
     });
 }
@@ -60,4 +61,5 @@ static NSArray *sSharedThunderAnimationFrames;
 {
     return sSharedThunderAnimationFrames;
 }
+
 @end
